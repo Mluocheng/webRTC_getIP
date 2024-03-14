124da90681a992aca63f4fcfb075c4ebd4e3a709
cheng <cheng@zbanx.com>
2024-03-14 09:59:18 +0800

    fix: 更新说明

--- README.md
+++ README.md
@@ -7,5 +7,8 @@ npm i -g node-stun
 node-stun-server
 
 ## 访问html 
-
+    注意：需要修改html中stun地址为你的stun地址
+    同一台电脑启动启动服务只能查到  0.0.0.0 / 127.0.0.1 的，
+    可以使用一些代理浏览器模拟客户端浏览器访问，就可以看到客户端ip:
+        ![alt text](image.png)
     
--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const stunPort = 3478; // 标准的STUN端口是 3478
 const server = stun.createServer();
 server.listen(stunPort);
 
-server.on('listening', () => {
-  console.log(`STUN server started on port ${stunPort}`);
+server.on('listening', (e) => {
+  console.log(`STUN server started on port ${stunPort}`, e);
 });
 
-server.on('bindingRequest', () => {
-  console.log('Received STUN binding request');
+server.on('bindingRequest', (e) => {
+
+  console.log('Received STUN binding request', e);
 });
